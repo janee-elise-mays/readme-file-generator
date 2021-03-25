3b6ccae175e978efeb3eb48263900de81d5cb6a2
Janeé Mays <janee.elise@gmail.com>
2021-03-24 21:13:57 -0400

    added prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = require('inquirer');
+    .prompt([
+        {
+            type: "input",
+            message: "What is the app for?",
+            name: "purpose",
+
+        },
+        {
+            type: "input",
+            message: "How to use the app?",
+            name: "use",
+        },
+        {
+            type: "input",
+            message: "How to install the app?",
+            name: "install",
+        },
+        {
+            type: "input",
+            message: "How to report issues?",
+            name: "issues",
+        },
+        {
+            type: "input",
+            message: "How to make contributions?",
+            name: "contribute",
+        }
+])
+.then(answers => {
+    JSON.stringify(answers);
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
