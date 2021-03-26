b71487aa9967f07dc8d50ae384bbf4d141c2fd7b
Janeé Mays <janee.elise@gmail.com>
2021-03-26 19:21:32 -0400

    revision to get questions prompts to properly deploy

--- Develop/index.js
+++ Develop/index.js
@@ -4,51 +4,104 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions =
     inquirer.prompt([
-        { type: "input", message: "What is the title of the project?", name: "title", },
-        { type: "input", message: "Briefly describe the project.", name: "description", },
-        { type: "input", message: "How to install the app?", name: "install", },
-        { type: "input", message: "How to use the app?", name: "use", },
-        { type: "input", message: "How to make contributions?", name: "contribute",},
-        { type: "input", message: "How to report issues?", name: "issues", },
-        { type: "input", message: "What is your github username?", name: "username", },
-        { type: "input", message: "What is your email?", name: "email", },
+        { type: "input", 
+        message: "What is the title of the project?", 
+        name: "title", 
+        },
+        { 
+            type: "input", 
+            message: "Briefly describe the project.", 
+            name: "description", 
+        },
+        { 
+            type: "input",
+             message: "How to install the project?", 
+             name: "install", 
+        },
+        { 
+            type: "input",
+             message: "How to use the project?", 
+             name: "use", 
+        },
+        { 
+            type: "input", 
+            message: "How to make contributions?", 
+            name: "contribute",
+        },
+        { 
+            type: "input", 
+            message: "How to report issues?", 
+            name: "issues", 
+        },
+        { 
+            type: "checkbox", 
+            message: "Select the license you used.",
+            name: "license", 
+            choices:[ "Mozilla Public License 2.0", "Apache License 2.0", "MIT License"],
+        },
+        { 
+            type: "input", 
+            message: "What is your github username?", 
+            name: "username", 
+        },
+        { 
+            type: "input", 
+            message: "What is your email?", 
+            name: "email", 
+        },
     ])
-        .then(answers => {
-            const readme = createREADME(answers);
-            fs.writeFileSync('./readme.md', readme, (error) => { if (error) console.log("Opps, try again") });
-        });
+    .then(answers => {
+        JSON.stringify(answers);
+    })
+        
 // JSON.stringify(answers);
 // console.log(answers);
 
 // TODO: Create a function to write README file
-function writeToFile({ answers }) {
-    // fileName, data
-    return
-    < !DOCTYPE md >
-
-    <h1># ${answers.title}</h1>
-    <h1># Table of Contents</h1>
-    <p>Description</p>
-    <p>Installation</p>
-    <p>Usage</p>
-    <p>Contributions</p>
-    <p>Tests</p>
-    <p>Questions</p>
-    <h1># Description</h1>
-    <p> ${answers.description}</p>
-    <h1># Installation</h1>
-    <p> ${answers.install}</p>
-    <h1># Usage</h1>
-    <p> ${answers.use}</p>
-    <h1># Contribution</h1>
-    <p> ${answers.contribute}</p>
-    <h1># Tests</h1>
-    <h1># Questions</h1>
-    <p> ${answers.purpose}</p>
-    
+// function writeToFile({ answers }) {
+//     // fileName, data
+//     return '# ${answers.title}
 
 
-}
+//     ## Description'
+//     ${answers.description}
+
+
+//     ## Table of Contents:
+//     [Installation](#installation)
+//     [Usage](#usage)
+//     [Contributions](#contributions)
+//     [Tests](#tests)
+//     [Questions](#questions)
+
+   
+//     ## Installation
+//     ${answers.install}
+
+
+//     ## Usage
+//     ${answers.use}
+
+
+//     ## Contributions
+//     ${answers.contribute}
+
+
+//     ## Tests
+//     ${answers.issues}
+
+
+//     ## Questions
+//     'If you have additional questions about this application, please contact me below using the methods below.'
+//     ${answers.username}
+//     ${answers.email}
+
+// '
+// }
+// .then(answers => {
+//     const readme = createREADME(answers);
+//     fs.writeFileSync('./readme.md', readme, (error) => { if (error) console.log("Opps, try again") });
+// });
 
 // TODO: Create a function to initialize app
 // function init() => {
@@ -57,4 +110,4 @@ function writeToFile({ answers }) {
 // };
 
 // Function call to initialize app
-init();
+// init();
